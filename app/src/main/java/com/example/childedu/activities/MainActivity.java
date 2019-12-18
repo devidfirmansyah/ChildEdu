4024fafa49db5fa464c231210af2de41d0329320
Devid <firmansyahdevid@gmail.com>
2019-12-18 15:59:10 +0700

    Tahap 3 : Membuat Fragment Home

--- app/src/main/java/com/example/childedu/activities/MainActivity.java
+++ app/src/main/java/com/example/childedu/activities/MainActivity.java
@@ -1,16 +1,53 @@
 package com.example.childedu.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.childedu.R;
+import com.example.childedu.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
